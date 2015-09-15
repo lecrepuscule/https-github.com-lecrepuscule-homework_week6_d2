b45d01b915afebac1a10b46445bed6aa713623d5
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-16 00:53:40 +0100

    added stock show action and drafts of new trade action

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,4 +8,20 @@ class ClientsController < ApplicationController
     @client = Client.find(params[:id])
   end
 
+  def new_trade
+    @client = Client.find(params[:id])
+    @trade = @client.trades.new
+  end
+
+  def create_trade
+    @client = Client.find(params[:id])
+    deal_id = @client.trades.max.deal_id + 1
+    @client.trade_portfolio(params[:portfolio], params[:quantity], deal_id)    
+  end
+
+  private
+  def trade_params
+    params.require(:trade).permit!
+  end
+
 end
